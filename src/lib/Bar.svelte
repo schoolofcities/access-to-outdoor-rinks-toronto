a29e4413725a33f414b27517413c5fbfda0bd38a
Jeff Allen <jeffallen89@gmail.com>
2023-10-24 12:17:02 -0400

    svelte setup, top image, start text

---
+++ src/lib/Bar.svelte
@@ -0,0 +1,106 @@
+<script>
+
+    import data from "../assets/data.json";
+
+    export let variable;
+
+    let filteredData = data.filter((item) => item.title === variable)[0];
+
+    let width;
+    let maxPercent = 23;
+
+    function roundToDecimal(number) {
+        return Math.round(number * 10) / 10;
+    }
+    
+    
+
+</script>
+
+
+<div class="bar" bind:clientWidth={width}>
+
+
+    <svg width="100%" height="7">
+        <!-- <text x="0" y="14" font-size="18" fill="#8DBF2E">{filteredData.title}</text> -->
+        <line id="borderline" x1="0" y1="2" x2="{width}" y2="2"></line>
+    </svg>
+
+    {#each filteredData.data as item}
+        
+        <svg width="100%" height="45">
+            
+            <text x="0" y="13" font-size="14" fill="white">{item.Name}</text>
+
+            <text x="5" y="35" font-size="14" fill="white" stroke="white"  stroke-width="2">{roundToDecimal(item.Evening)}%</text>
+            <text x="{(item.Evening / maxPercent) * width + 5}" y="35" font-size="14" fill="white" stroke="white" stroke-width="2">{roundToDecimal(item.Night)}%</text>
+
+            <rect id="eveningbar" width="{(item.Evening / maxPercent) * width}" height="20" x="0" y ="20"/>
+            <rect id="nightbar" width="{(item.Night / maxPercent) * width}" height="20" x="{(item.Evening / maxPercent) * width}" y ="20"/>
+            <rect id="bardivider" width="2" height="20" x="{(item.Evening / maxPercent) * width - 1}" y ="20"/>
+
+            <text x="5" y="35" font-size="14" fill="black">{roundToDecimal(item.Evening)}%</text>
+            <text x="{(item.Evening / maxPercent) * width + 5}" y="35" font-size="14" fill="black">{roundToDecimal(item.Night)}%</text>
+
+        </svg>
+
+    {/each}
+
+    <svg width="100%" height="105">
+        <line id="xaxis" x1="0" y1="1" x2="{width}"  y2="1"></line>
+
+        <line id="xaxis" x1="1" y1="1" x2="1"  y2="8"></line>
+        <line id="xaxis" x1="{width - 1}" y1="1" x2="{width - 1}"  y2="8"></line>
+        <line id="xaxis" x1="{width * 0.25}" y1="1" x2="{width * 0.25}"  y2="8"></line>
+        <line id="xaxis" x1="{width * 0.5}" y1="1" x2="{width * 0.5}"  y2="8"></line>
+        <line id="xaxis" x1="{width * 0.75}" y1="1" x2="{width * 0.75}"  y2="8"></line>
+
+        <text x="0" y="21" font-size="14" fill="#D0D1C9" text-anchor="left">0%</text>
+        <text x="{width * 0.25}" y="21" font-size="14" fill="#D0D1C9" text-anchor="middle">5%</text>
+        <text x="{width * 0.5}" y="21" font-size="14" fill="#D0D1C9" text-anchor="middle">10%</text>
+        <text x="{width * 0.75}" y="21" font-size="14" fill="#D0D1C9" text-anchor="middle">15%</text>
+        <text x="{width}" y="21" font-size="14" fill="#D0D1C9" text-anchor="end">20%</text>
+
+        <text x="{width / 2}" y="40" font-size="14" fill="#D0D1C9" text-anchor="middle">Percent of all journey-to-work trips</text>
+
+        <rect id="eveningbar" width="10" height="20" x="0" y ="45"/>
+        <text x="15" y="60" font-size="14" fill="#D0D1C9" text-anchor="left">Evening Shifts</text>
+
+        <rect id="nightbar" width="10" height="20" x="110" y ="45"/>
+        <text x="125" y="60" font-size="14" fill="#D0D1C9" text-anchor="left">Night Shifts</text>
+
+    </svg>
+
+</div>
+
+
+<style>
+
+    .bar {
+        max-width: 750px;
+        width: 100%;
+        min-height: 20px;
+        background-color: none;
+        margin: 0px;
+        padding: 0px;
+        margin-bottom: 5px;
+    }
+    #eveningbar {
+        fill: var(--brandLightBlue);
+    }
+    #nightbar {
+        fill: var(--brandYellow);
+    }
+    #bardivider {
+        fill: var(--brandWhite);
+    }
+    #borderline {
+        stroke: var(--brandPink);
+        stroke-width:1
+    }
+    #xaxis {
+        stroke: var(--brandGray);
+        stroke-width:1
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+
+    import TopSofC from "../lib/TopSofC.svelte";
+    import "../assets/global-styles.css";
+    import topImage from '../assets/top-image.png'
+    
+</script>
+
+<TopSofC/>
+
+<main>
+    <div class="topImg" style="background-image: url({topImage});">
+        
+    </div>
+    
+    <div class="text">
+        <div class="title">
+            <h1>The Geography of Toronto's Outdoor Skating Rinks</h1>
+            <p>Jeff Allen & Teresa Lau - November 2023</p>
+        </div>
+        <p>
+            Cities with colder climates, like many in Canada, 
+
+
+            - Why skating rinks are important, cite 2-4 pieces of key literature/new articles
+            - Briefly on history of skating rinks in Toronto, how many are there? what do they offer (public skating/hockey/classes/can be booked for activities)
+        </p>
+        <p>
+            While there are over 50 outdoor rinks in Toronto, there are large swaths of the City that do not live close to a rink and thus have to travel long distances to access them.
+        </p>
+        <p>
+            Below we've created a map showing the locations of outdoor skating rinks, with the background coloured by the travel time in each neighbourhood to the nearest skating rink. This is a type of <a href="">isochrone map</a>. 
+        </p>
+
+        <p>INSERT LARGE HEX MAP HERE</p>
+
+        <p>
+            Briefly describe map here, patterns, etc.
+        </p>
+
+
+        <h2>Who has access?</h2>
+       
+        <p>
+            Describe ...
+        </p>
+
+        <p>INSERT 4 SMALL CENSUS TRACT MAPS HERE</p>
+
+        <p>
+            Describe maps
+        </p>
+
+        <p>
+            INSERT BAR CHARTS HERE
+        </p>
+        
+    </div>
+
+</main>
+
