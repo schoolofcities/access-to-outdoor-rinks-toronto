d1b7c15543dd1aba99d3bd273a95d203c2e0043c
Jeff Allen <jeffallen89@gmail.com>
2023-10-25 17:03:20 -0400

    get main map going with hex

--- src/lib/HexAccessMap.svelte
+++ src/lib/HexAccessMap.svelte
@@ -3,9 +3,14 @@
 	import { onMount } from 'svelte';
 	import maplibregl from 'maplibre-gl';
 	import "../assets/maplibre-gl.css";
+	import rinks from '../assets/toronto-rinks.geo.json';
+	import municipalBoundaries from '../assets/toronto-former-municipal-boundaries.geo.json';
+	import hexGrid from '../assets/toronto-hex-grid.geo.json';
+	import notToronto from '../assets/toronto-not.geo.json';
 
 	let pageHeight;
 	let pageWidth;
+	let map;
 
 	let mapHeight = 600;
 	$: if (pageHeight < 800) {
@@ -16,15 +21,13 @@
 
 	onMount(() => {
 
-		const maxBounds = 
-
 		map = new maplibregl.Map({
 			container: 'map',
 			// style: './vector-tiles-vintage-v4.json',//'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
-			center: [-79.38, 43.71], // starting position
+			center: [-79.36, 43.715], // starting position
 			zoom: 10.5, // starting zoom;
 			minZoom: 9,
-			maxZoom: 14,
+			maxZoom: 13,
 			bearing: -17,
 			maxBounds:[ 
 				[-80.28, 43.21], 
@@ -45,27 +48,83 @@
 		map.touchZoomRotate.disableRotation();
 
 
+		map.addSource('hexGrid', {
+			type: 'geojson',
+			data: hexGrid
+		})
+		map.addLayer({
+			'id': 'hexGrid',
+			'type': 'fill',
+			'source': 'hexGrid',
+			'paint': {
+				'fill-color': [
+					'step',
+					['get', 'id'],
+					'#e7f8ff', 10000, 
+					'#8dd3ef', 20000, 
+					'#6e8ac0', 30000, 
+					'#6d559c', 40000,
+					'#6d247a' 
+				],
+				'fill-opacity': 0.75,
+				'fill-outline-color': 'white',
+			}
+		})
 
-		map.addSource('raster-tiles', {
+		map.addSource('notToronto', {
+			type: 'geojson',
+        	data: notToronto
+		})
+		map.addLayer({
+			'id': 'notToronto',
+			'type': 'fill',
+			'source': 'notToronto',
+			'paint': {
+				'fill-color': '#ffffff',
+				'fill-opacity': 1
+			}
+		})
+
+		map.addSource('osm-raster-tiles', {
 			'type': 'raster',
 			'tiles': ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
 			'tileSize': 256,
 			'minzoom': 0,
 			'maxzoom': 19
-        });
-
+		});
 		map.addLayer({
-			'id': 'simple-tiles',
+			'id': 'osm-raster-tiles',
 			'type': 'raster',
-			'source': 'raster-tiles',
+			'source': 'osm-raster-tiles',
 			'paint': {
 				'raster-saturation': -1,
-				'raster-opacity': 0.15
+				'raster-opacity': 0.13
+			}
+		})	
+
+		map.addSource('municipalBoundaries', {
+			type: 'geojson',
+        	data: municipalBoundaries
+		})
+		map.addLayer({
+			'id': 'municipalBoundaries',
+			'type': 'line',
+			'source': 'municipalBoundaries',
+			'paint': {
+				'line-color': '#007FA3'
 			}
 		})
 
+		map.addSource('rinks', {
+				type: 'geojson',
+				data: rinks
+			})
+			map.addLayer({
+				'id': 'rinks',
+				'type': 'circle',
+				'source': 'rinks',
+			})
 
-	
 	});
 
 </script>
@@ -75,13 +134,12 @@
 
 <svelte:window bind:innerHeight={pageHeight} bind:innerWidth={pageWidth}/>
 
+<p>
+	15, 30, 45, 60
+</p>
 
+<div id="map" style="height: {mapHeight}px"></div>
 
-
-
-<div id="map" style="height: {mapHeight}px">
-
-</div>
 <p>Data Sources: OpenStreetMap, City of Toronto</p>
 
 
